08fa57891d167a35c2b1b083c6d139da081e2a0b
Yevhenii-Melnyk <evgm1991@gmail.com>
2021-04-29 17:15:15 +0300

    new day

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-jQuery(document).ready(function ($) {
+jQuery(document).ready(function($) {
+  const $burger = $(".header__burger");
+  const $nav = $(".header__nav");
+  const $html = $("html");
+  const $a = $(".header__nav a");
 
+  $burger.on("click", function() {
+    $nav.toggle();
+    $html.toggleClass("overflow");
+  });
+
+  $a.on("click", function() {
+    $nav.hide();
+    $html.removeClass("overflow");
+  });
 });
