53005eca92c4b68e199aceb9120de481950ba32c
Yevhenii-Melnyk <evgm1991@gmail.com>
2021-05-04 00:16:39 +0300

    Upgrade

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ jQuery(document).ready(function($) {
   const $nav = $(".header__nav");
   const $html = $("html");
   const $a = $(".header__nav a");
+  const $arrowUp = $(".arrowUp");
+  const $headerNav = $(".header__nav a");
 
   $burger.on("click", function() {
     $nav.toggle();
@@ -12,5 +14,27 @@ jQuery(document).ready(function($) {
   $a.on("click", function() {
     $nav.hide();
     $html.removeClass("overflow");
+  });
+
+  $(window).on(
+    "scroll",
+    $.debounce(500, function() {
+      if ($(this).scrollTop() > 100) {
+        $arrowUp.addClass("active");
+      } else {
+        $arrowUp.removeClass("active");
+      }
+    })
+  );
+
+  $arrowUp.on("click", function() {
+    $("html, body").animate({ scrollTop: 0 }, 700);
+  });
+
+  $headerNav.on("click", function(e) {
+    e.preventDefault();
+    const target = $(this).attr("href");
+    console.log($(target).offset().top);
+    $("html, body").animate({ scrollTop: $(target).offset().top }, 500);
   });
 });
